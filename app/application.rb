83e5962c92f6d633c738b6c8b4e30e262db763ab
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-26 14:05:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,8 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@item.find{|i| i.name == item_name}
-      binding.pry
       
-      if item = @@item.find{|i| i.name == item_name}
+      if item !nil
         resp.write item.price
       else
         resp.write "Item not found"
